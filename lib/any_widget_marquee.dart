d52764e36866f113f9e90ab12fbf85f689829289
MobileMon <MobileMon@users.noreply.github.com>
2020-08-05 08:02:57 -0400

    Update any_widget_marquee.dart
    Wrapping Entire widget in ExcludeSemantics to stop crash caused by a Flutter bug
    
    issue discussed here: https://github.com/THALLIVA/any_widget_marquee/issues/1

--- lib/any_widget_marquee.dart
+++ lib/any_widget_marquee.dart
@@ -9,7 +9,9 @@ class AnyMargueeSpeed {
 }
 
 class AnyMargueeWidget extends StatefulWidget {
-  AnyMargueeWidget({@required this.child, Key key, this.marginLeft, this.betweenSpacing, this.width, this.height, this.speedRate = 1}) : super(key: key);
+  AnyMargueeWidget(
+      {@required this.child, Key key, this.marginLeft, this.betweenSpacing, this.width, this.height, this.speedRate = 1})
+      : super(key: key);
 
   double marginLeft;
   double betweenSpacing;
@@ -38,42 +40,46 @@ class _AnyMargueeWidgetState extends State<AnyMargueeWidget> {
   void startTimer() {
     _anyMargueeTimer = Timer.periodic(Duration(microseconds: 16), (timer) {
       final distance = _scrollController.offset ?? 0;
-      if (_scrollController.hasClients) _scrollController.jumpTo(distance + (1 / AnyMargueeSpeed.NORMAL_SPEED) * widget.speedRate);
+      if (_scrollController.hasClients) _scrollController.jumpTo(
+          distance + (1 / AnyMargueeSpeed.NORMAL_SPEED) * widget.speedRate);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return ScrollNotificationInterceptor(
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          final biggestWidth = constraints.biggest.width;
-          final biggestHeight = constraints.biggest.height;
-          widget.marginLeft ??= biggestWidth;
-          widget.betweenSpacing ??= biggestWidth;
-          widget.width ??= biggestWidth;
-          widget.height ??= biggestHeight;
+    return ExcludeSemantics(
+      child: ScrollNotificationInterceptor(
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            final biggestWidth = constraints.biggest.width;
+            final biggestHeight = constraints.biggest.height;
+            widget.marginLeft ??= biggestWidth;
+            widget.betweenSpacing ??= biggestWidth;
+            widget.width ??= biggestWidth;
+            widget.height ??= biggestHeight;
 
-          return Container(
-            width: widget.width,
-            height: widget.height,
-            alignment: Alignment.center,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: double.maxFinite.toInt(),
-              itemBuilder: (context, index) {
-                final distance = (index == 0 ? widget.marginLeft : widget.betweenSpacing);
-                return Container(
-                  padding: EdgeInsets.only(left: distance),
-                  alignment: Alignment.center,
-                  child: widget.child,
-                );
-              },
-              controller: _scrollController,
-              physics: NeverScrollableScrollPhysics(),
-            ),
-          );
-        },
+            return Container(
+              width: widget.width,
+              height: widget.height,
+              alignment: Alignment.center,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: double.maxFinite.toInt(),
+                itemBuilder: (context, index) {
+                  final distance = (index == 0 ? widget.marginLeft : widget
+                      .betweenSpacing);
+                  return Container(
+                    padding: EdgeInsets.only(left: distance),
+                    alignment: Alignment.center,
+                    child: widget.child,
+                  );
+                },
+                controller: _scrollController,
+                physics: NeverScrollableScrollPhysics(),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
